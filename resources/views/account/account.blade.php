949e5f95ca03b69ca67a351b590b43128e29f1c2
FLTOki <jim.musyoki@strathmore.edu>
2021-06-02 18:52:03 +0300

    initial commit

---
+++ resources/views/account/account.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <!-- {{ __('Dashboard') }} -->
+            Home
+        </h2>
+    </x-slot>
+
+    <main>
+        <div class="intro">
+            <div class="title">
+                <p class="title"><strong>NYUMBA YANGU</strong></p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                </svg>
+            </div>
+        </div>
+    </main>
+    
+
+    <!-- <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">               
+            </div>
+        </div>
+    </div> -->
+</x-app-layout>
